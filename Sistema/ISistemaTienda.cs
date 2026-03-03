7a3520af352212d39ac676d16f91ad04b18ca6b7
ivanmorales <ivanandres010605@gmail.com>
2026-03-02 20:07:49 -0500

    commit

--- Sistema/ISistemaTienda.cs
+++ Sistema/ISistemaTienda.cs
@@ -2,13 +2,22 @@
 {
     internal interface ISistemaTienda
     {
-        // Caja
+        // Caja venta
         void RegistrarVenta(int idProducto, int cantidad);
+        void CobrarPago(int idProducto, int cantidad);
+        void GenerarFactura(int idProducto, int cantidad);
 
         // Bodega
         void ActualizarInventario(int idProducto, int cantidad);
 
+        // Supervisor  Reportes  Devoluciones
+        void ProcesarDevolucion(int idVenta);
+        void GenerarReporteDiario();
+
+
         // Soporte
         void AtenderReclamo(int idReclamo);
+
+
     }
 }
